637837e5be1a64056f81aa7131c1b56885267413
Abhinav Prakash <f20180583@pilani.bits-pilani.ac.in>
2021-10-11 15:57:08 +0530

    Update reading.py

--- TestExcel/reading.py
+++ TestExcel/reading.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
 def inn(var1,var2):
-    cond1 = df.loc[:, "Segment"] == var1
-    cond2 = df.loc[:, "Country"] == var2
+    cond1 = df.loc[:, "MO"] == var1
+    cond2 = df.loc[:, "Parameter ID"] == var2
     a = df.loc[cond1 & cond2]
-    x = a.loc[:,"Product"].sample(n=1).values[0]
+    x = a.loc[:,"MML Command"].values[0]
     out(x)
 
 def out(x):
     with open('testing.txt', 'r') as file:
         line = file.readlines()
-        line[2] = '%%/*28941843*/' + x + ':;%%'
+        line[2] = '%%/*28941843*/' + x.strip() + ':;%%'
         for i in line:
             print(i)
 
-df = pd.read_excel('testing.xlsx')
+df = pd.read_excel('telecom.xlsx')
 
 var1 = input("Enter the MO : ")
 var2 = input("Enter the Parameter ID : ")
